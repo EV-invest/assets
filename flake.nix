1d97b25d6e105b0b84b50d1412936e717841f07f
Valera <valeratrades@gmail.com>
2026-07-09 15:11:49 +0000

    _

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/da5ad661ba4e5ef59ba743f0d112cbc30e474f32";
     flake-utils.url = "github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b";
     v_flakes.url = "github:valeratrades/v_flakes/6062f652effc94be053865d58ff03c697c31ecb6";
+    v_flakes.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils, v_flakes }:
@@ -11,10 +12,14 @@
         pkgs = import nixpkgs { inherit system; };
         accent = (import ./logo/logo_colors.nix).accent_color;
         # dark: accent bg + white logo; light: white bg + accent logo. ($accent is build-time.)
+        # 175 matches the old 370/640 proportion, - smallest reasonable fit.
         render = name: fill: bg: ''
           sed "s/black/${fill}/g" logo/logo.svg > ${name}.svg
-          resvg --width 370 ${name}.svg ${name}-content.png
-          magick ${name}-content.png -background "${bg}" -gravity center -extent 640x640 -flatten logo.${name}.png
+          for size in 175 225 275; do
+            resvg --width $size ${name}.svg ${name}-content.png
+            dims=$(magick identify -format "%wx%h" ${name}-content.png)
+            magick ${name}-content.png -background "${bg}" -gravity center -extent 300x300 -flatten "logo.300x300-$dims.${name}.png"
+          done
         '';
       in
       {
@@ -28,7 +33,7 @@
             ${render "dark" "white" "$accent"}
             ${render "light" "$accent" "white"}
           '';
-          installPhase = "install -Dm644 logo.dark.png logo.light.png -t $out";
+          installPhase = "install -Dm644 logo.300x300-*.png -t $out";
         };
 
         devShells.default = pkgs.mkShell {
